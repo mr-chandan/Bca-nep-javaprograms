da17f3371df3a34b78642e54f33085fe1cfcafb1
Chandan H <106821717+mr-chandan@users.noreply.github.com>
2022-09-10 17:03:31 +0530

    Update Program_20.java

--- Program_20.java
+++ Program_20.java
@@ -18,7 +18,7 @@ public class Program_20 extends Applet implements ActionListener {
 
     public void actionPerformed(ActionEvent e) {
         s1 = "Full Name: Bam";
-        s2 = "Father Name: Som Mother Name: Kamalamma Age: 45";
+        s2 = "Father Name: Som Mother Name: Sum Age: 45";
         s3 = "Roll No : 431 College Name: Surana College";
         s4 = "Nationality: Indian Contact No: 0000000000";
         s5 = "xyz;
